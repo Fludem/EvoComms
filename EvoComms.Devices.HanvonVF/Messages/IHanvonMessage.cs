2cd2b20fae211120edcefb7026ff661e06001d35
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-11-24 01:06:37 +0000

    refactor: remove unused Hanvon message classes/interfaces
    Eliminated `HanvonMessage`, `IHanvonMessage`, and `IHanvonResponse` due to refactoring.
    
    Signed-off-by: Dylan Marriott <dylan@onyxleeds.co.uk>

--- EvoComms.Devices.HanvonVF/Messages/IHanvonMessage.cs
+++
@@ -1,7 +0,0 @@
-namespace EvoComms.Devices.HanvonVF.Messages;
-
-public interface IHanvonMessage<TParam>
-{
-    string COMMAND { get; }
-    TParam PARAM { get; }
-}
--- EvoComms.Devices.HanvonVF/Messages/IHanvonResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace EvoComms.Devices.HanvonVF.Messages;
-
-public interface IHanvonResponseMessage<TParam>
-{
-    string RETURN { get; }
-    TParam PARAM { get; }
-}
