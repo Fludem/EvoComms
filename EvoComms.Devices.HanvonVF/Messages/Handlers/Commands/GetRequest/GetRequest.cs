7d4def3262e840e246c122209701df74342439a5
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-11-24 01:07:50 +0000

    feat: add GetRequest command handler
    Introduced a handler for the GetRequest command to manage incoming requests for serial numbers.
    
    Signed-off-by: Dylan Marriott <dylan@onyxleeds.co.uk>

---
+++ EvoComms.Devices.HanvonVF/Messages/Handlers/Commands/GetRequest/GetRequest.cs
@@ -0,0 +1,37 @@
+using EvoComms.Devices.HanvonVF.Messages.Handlers.Commands.Attributes;
+using Microsoft.Extensions.Logging;
+using Mina.Core.Session;
+using Newtonsoft.Json;
+
+namespace EvoComms.Devices.HanvonVF.Messages.Handlers.Commands.GetRequest;
+
+public class GetRequestCommand : BaseHanvonCommand
+{
+    [JsonProperty("PARAM")] public GetRequestParams Parameters { get; set; }
+}
+
+[HanvonCommandHandler("GetRequest")]
+public class GetRequestHandler : IHanvonMessageHandler
+{
+    private readonly ILogger<GetRequestHandler> _logger;
+
+    public GetRequestHandler(ILogger<GetRequestHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task HandleMessageAsync(IoSession session, object message)
+    {
+        _logger.LogInformation("Handling GetRequest");
+        var command = message as GetRequestCommand;
+        if (command != null)
+            // Handle the GetRequest command
+            _logger.LogInformation($"Handling GetRequest for SN: {command.Parameters.SerialNumber}");
+        return Task.CompletedTask;
+    }
+}
+
+public class GetRequestParams
+{
+    [JsonProperty("SN")] public required string SerialNumber { get; set; }
+}
