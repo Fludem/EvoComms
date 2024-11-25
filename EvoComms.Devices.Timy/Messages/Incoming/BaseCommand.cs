40bd6b930a4c8725810297b13c5243d65f0404c9
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-11-25 05:32:40 +0000

    feat(devices): add BaseCommand class for incoming messages
    Introduce BaseCommand class with JSON serialization for incoming messages in Timy device.
    
    Signed-off-by: Dylan Marriott <dylan@onyxleeds.co.uk>

---
+++ EvoComms.Devices.Timy/Messages/Incoming/BaseCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EvoComms.Devices.Timy.Messages.Incoming;
+
+public class BaseCommand
+{
+    [JsonPropertyName("ret")] public required string Command { get; set; }
+
+    protected string GetTimeString()
+    {
+        return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+    }
+}
