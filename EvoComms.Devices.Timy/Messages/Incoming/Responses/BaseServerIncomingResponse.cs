c12bff2fc4795a222e46cbc25e77f49cb4f77c7e
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-11-25 22:50:39 +0000

    refactor: remove deprecated Timy message classes
    Removed unused base response and request classes for cleaner code.
    
    Signed-off-by: Dylan Marriott <dylan@onyxleeds.co.uk>

--- EvoComms.Devices.Timy/Messages/Incoming/Responses/BaseServerIncomingResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace EvoComms.Devices.Timy.Messages.Incoming.Responses;
-
-public class BaseServerIncomingResponse : BaseServerResponse
-{
-    [JsonPropertyName("sn")] public required string SerialNumber { get; set; }
-}
